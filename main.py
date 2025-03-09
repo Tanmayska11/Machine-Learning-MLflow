beb72f22e88ae5b8adf2aea5d74b7a174feaf25b
Tanmay Khairnar <tanmayska11@gmail.com>
2025-03-09 13:47:13 +0100

    Data injection added

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from mlproject import logger
 #from src.mlproject import logger
+from mlproject.pipeline.stage_01_data_ingestion import DataInjectioTrainingPipeline
 
+STAGE_NAME = "Data Injection stage"
 
-logger.info("welcome to our custom logger")
+try:
+    logger.info(f">>>>>>>>>{STAGE_NAME} started<<<<<<<<")
+    data_injection = DataInjectioTrainingPipeline()
+    data_injection.main()
+    logger.info(f">>>>>>>>>{STAGE_NAME} completed<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
